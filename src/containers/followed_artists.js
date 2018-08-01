953c0e199a2f3054b2b980b4934b120b6178019c
Dexter Collins <dexter_collins@abercrombie.com>
2018-08-01 15:55:54 -0400

    Created follow_artists component file

---
+++ src/containers/followed_artists.js
@@ -0,0 +1,9 @@
+//The container that will render a list of artists.
+// - ActionCreators: Will call an action creator that will update the state with the user's spotify followed artists.
+//   This will be fetched during componentDidMount
+
+//Import modules
+
+class followedArtists extends Component {
+    
+}
