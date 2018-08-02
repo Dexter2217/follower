05e5424b99d1acd14d6ba4b42aca3a9bad39b192
Dexter <dexter2217@gmail.com>
2018-08-01 23:51:48 -0400

    - Stubbed followed_artists.js component - Stubbed fetchFollowedArtists function in actions/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//Declare constants that will be the 'type' in each actionCreator sent by functions
+//defined in this file
+
+export const FETCH_FOLLOWED_ARTISTS = "fetch_followed_artists";
+
+export function fetchFollowedArtists () {
+    //Make axios API call
+
+    //Return actionCreator object that gets sent to the reducer
+}
---
+++ src/components/followed_artists.js
@@ -0,0 +1,42 @@
+//The container that will render a list of artists.
+// - ActionCreators: Will call an action creator that will update the state with the user's spotify followed artists.
+//   This will be fetched during componentDidMount
+
+//Import modules
+import _ from "lodash";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchFollowedArtists } from "../actions";
+
+
+class followedArtists extends Component {
+    componentDidMount () {
+        //Fetch the data via this.props.fetchFollowedArtists
+        console.log("fetched followed artists:");
+        console.log(this.props.fetchFollowedArtists());
+    }
+    renderArtists () {
+        //Return concatenated <li>'s for each artist
+        //Access the followed_artists state property via
+        //this.props.followed_artists
+
+        //Loop through each artist and return an <li> for each one
+
+        return _.map(this.props.followedArtists, artist => {
+            return (
+                <li key={artist.name}>{artist.name}</li>
+            );
+        });
+    }
+
+    render () {
+        //returns DOM wrapper and calls this.renderArtists()
+
+        return ( 
+            <ul className="list-group">
+                {this.renderArtists()}
+            </ul>
+        );
+    }
+    
+}
